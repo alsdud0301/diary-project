20aedf8dd5adb0fbf6652458a9e478b4cba15aa9
alsdud0301 <sms0751@naver.com>
2024-07-21 15:24:55 +0900

    page.tsx 수정2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
 'use client'
 import React, { useEffect } from 'react';
-import Login from "@/components/Login"
-import { AuthProvider } from '../context/AuthContext';
 import { useRouter } from 'next/navigation';
 
 const Home: React.FC = () => {
